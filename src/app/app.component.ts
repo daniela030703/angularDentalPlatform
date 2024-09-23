0c185981781c1c3c6cf8b3a31686d4c1ed751072
Daniela <danielachirivim@gmail.com>
2024-09-22 19:32:32 -0500

    ajuste signIn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { UsComponent } from './webPage/us/us.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FooterComponent, HomeComponent, UsComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './webPage/home/home.component';
 import { UsComponent } from './webPage/us/us.component';
+import { SingInComponent } from './modules/auth/sing-in/sing-in.component';
+import path from 'path';
+import { Component } from '@angular/core';
+import { WebPageComponent } from './webPage/web-page.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'us', component: UsComponent },
+    { path: '', component: WebPageComponent,
+        children: [
+            {
+                path: '',
+                component: HomeComponent
+            },
+            { path: 'us', 
+              component: UsComponent
+            },
+        ]
+     },
+   
+    { path: 'signIn', component: SingInComponent },
+
+
 ];
